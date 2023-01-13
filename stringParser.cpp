51cbdaaccb0ac90d6c05cf114c229923b4e9f7af
JosephK2006 <jiyong.kwon@stu.ocsb.ca>
2023-01-13 02:12:49 +0000

    added stringParser.cpp

---
+++ stringParser.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2022 Immaculata HS All rights reserved.
+//
+// Created by: Joseph Kwon
+// Created on: December 14 2022
+// This program generates 10 random
+// numbers and display the smallest
+
+// Including modules
+#include <time.h>
+
+#include <iostream>
+#include <list>
+using std::string;
+
+// Defining the function for the parsing
+void DisplayWordsWithoutSpaces(std::string aString) {
+    std::string word = "";
+    for (auto iter : aString) {
+        if (iter == ' ') {
+            std::cout << word << "\n";
+            word = "";
+        } else {
+            word = word + iter;
+        }
+    }
+    std::cout << word << "\n";
+}
+
+// Defining the main function
+int main() {
+    // Defining the variable
+    std::string userInput;
+
+    // Getting user input
+    std::cout << "Enter a sentence: ";
+    std::getline(std::cin, userInput);
+
+    // Calling the function
+    DisplayWordsWithoutSpaces(userInput);
+}
